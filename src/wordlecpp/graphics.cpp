71364f6ede9dd3cec7f010abf612e7494c0ab4a5
NoName24 <146374556+gNoName24@users.noreply.github.com>
2025-12-05 22:12:27 +0500

    Добавлена проверка в graphics на nullptr

--- src/wordlecpp/graphics.cpp
+++ src/wordlecpp/graphics.cpp
@@ -19,17 +19,25 @@ namespace WordleCPP {
         void init() {
             setlocale(LC_ALL, "");
             nc = notcurses_init(nullptr, stdout);
+            if(!nc) {
+                throw std::runtime_error("Ошибка инициализации Notcurses. Одна из возможных причин - Notcurses не установлен или консоль не поддерживает Notcurses");
+                return;
+            }
             ncmp = notcurses_stdplane(nc);
+            if(!ncmp) {
+                throw std::runtime_error("Ошибка получения stdplane");
+                return;
+            }
 
             unsigned int rows, cols;
             ncplane_dim_yx(ncmp, &rows, &cols);
 
             // Создание сцен
-            struct ncplane_options opts_scenes = {
+            struct ncplane_options newscene_opts = {
                 .y = 0, .x = 0,
                 .rows = rows, .cols = cols,
             };
-            scenes[SCENE_PROFILE_ENTRY] = ncplane_create(ncmp, &opts_scenes);
+            scenes[SCENE_PROFILE_ENTRY] = ncplane_create(ncmp, &newscene_opts);
         }
         void shutdown() {
             notcurses_stop(nc);
